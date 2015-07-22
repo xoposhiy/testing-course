aaa328a6b8cd7007e3ccbd7bd66ec533cd5ae251
Pavel Egorov <xoposhiy@mail.ru>
2015-07-23 01:31:39 +0500

    Add better task for antipatterns

---
+++ Patterns/Exercise/TextDocument.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontur.Courses.Testing.Patterns.Exercise
+{
+	class TextDocument
+	{
+		public string Text { get; private set; }
+		public int EditsCount { get; private set; }
+
+		public TextDocument(string text)
+		{
+			Text = text;
+		}
+
+		public ICommand MakeReplaceCommand(int startIndex, int len, string newText)
+		{
+			return new ReplaceCommand(this, startIndex, len, newText);
+		}
+
+		class ReplaceCommand : ICommand
+		{
+			private readonly TextDocument textDocument;
+			private readonly int startIndex;
+			private readonly int len;
+			private readonly string newText;
+			private readonly string oldText;
+
+			public ReplaceCommand(TextDocument textDocument, int startIndex, int len, string newText)
+			{
+				this.textDocument = textDocument;
+				this.startIndex = startIndex;
+				this.len = len;
+				this.newText = newText;
+				this.oldText = textDocument.Text.Substring(startIndex, len);
+			}
+
+			public void Do()
+			{
+				if (textDocument.Text.Substring(startIndex, len) != oldText)
+					throw new InvalidOperationException();
+				textDocument.Text = textDocument.Text.Substring(0, startIndex)
+					+ newText + textDocument.Text.Substring(startIndex + len);
+				textDocument.EditsCount++;
+			}
+
+			public void Undo()
+			{
+				if (textDocument.Text.Substring(startIndex, newText.Length) != newText)
+					throw new InvalidOperationException();
+				textDocument.Text = textDocument.Text.Substring(0, startIndex)
+					+ oldText + textDocument.Text.Substring(startIndex + newText.Length);
+				textDocument.EditsCount--;
+			}
+		}
+	}
+
+
+	interface ICommand
+	{
+		void Do();
+		void Undo();
+	}
+}
---
+++ Patterns/Exercise/TextDocumentTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Kontur.Courses.Testing.Patterns.Exercise
+{
+	[TestFixture]
+	public class TextDocumentTests
+	{
+		[Test]
+		public void Test()
+		{
+			var doc = new TextDocument("hello world");
+			var cmd = doc.MakeReplaceCommand(0, 1, "H");
+			cmd.Do();
+			Assert.AreEqual("Hello world", doc.Text);
+			var cmd2 = doc.MakeReplaceCommand(6, 5, "all!");
+			cmd2.Do();
+			Assert.AreEqual("Hello all!", doc.Text);
+			Assert.AreEqual(2, doc.EditsCount);
+			cmd2.Undo();
+			Assert.AreEqual(1, doc.EditsCount);
+			cmd.Undo();
+			Assert.AreEqual(0, doc.EditsCount);
+			Assert.AreEqual("hello world", doc.Text);
+			try
+			{
+				cmd2.Undo();
+				Assert.Fail("Should not be able to undo command which was not executed before");
+			}
+			catch (InvalidOperationException)
+			{
+				Console.WriteLine("Exception was thrown!");
+			}
+		}
+	}
+}
