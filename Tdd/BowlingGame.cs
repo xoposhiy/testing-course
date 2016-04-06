a0a99efc3aa36ab0ab2a89a0bfd28359958c185c
Pavel Egorov <xoposhiy@mail.ru>
2016-04-06 11:50:04 +0500

    New BowlingGame Api

--- Tdd/BowlingGame.cs
+++ Tdd/BowlingGame.cs
@@ -5,6 +5,10 @@ using NUnit.Framework;
 
 namespace Kontur.Courses.Testing.Tdd
 {
+	public class Frame
+	{
+	}
+
 	public class Game
 	{
 		public void Roll(int pins)
@@ -12,11 +16,14 @@ namespace Kontur.Courses.Testing.Tdd
 			
 		}
 
-		public int GetScore()
+		public IList<Frame> GetFrames()
 		{
-			return 0;
+			throw new NotImplementedException();
 		}
+
+		public int Score {  get { throw new NotImplementedException(); } }
 	}
+
 
 	[TestFixture]
 	public class BowlingGame_GetScore_should
